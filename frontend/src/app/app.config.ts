d7ceafcebf55ccd94ae64987f71debae05064d8b
Jhostin <shoockwaveeyt@gmail.com>
2026-09-11 15:22:26 -0500

    fix: configurar proxy inverso en nginx y angular para docker

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { BASE_PATH } from './api/variables';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: BASE_PATH, useValue: '' }
   ]
 };
